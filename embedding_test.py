128033cd962964f01206550a52c5e5dfcd321d48
mjkweon17 <mjkweon17@korea.ac.kr>
2024-03-22 21:09:32 +0900

    feat: embedding하는 함수 구현

---
+++ embedding_test.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from config import OpenAI_Settings
+from count_token import get_encoding
+
+client = OpenAI(
+    api_key=OpenAI_Settings.openai_api_key
+)
+
+
+def get_embedding(text: str):
+    result = client.embeddings.create(
+        model="text-embedding-3-large",
+        # 토큰의 최대 개수인 8192 토큰을 넘으면 안됨. empyty string도 안됨. 배열은 2048차원이거나 적어야 함.
+        input=text,
+        encoding_format="float"  # float 또는 base64
+    )
+    return result
